d148939784c87a3196267d45943cebf047d47609
mkadayifci <mkadayifci@gmail.com>
2016-11-19 18:24:19 +0000

    Write to log file

--- TestDive.py
+++ TestDive.py
@@ -1,19 +1,18 @@
-# Qucik test file to open TSY01 connection and reads its temp
-# 20160819 IMD
-#
-#
+
 
 import os
 import TSYS01
 import time
+import datetime
 
 sensor=TSYS01.TSYS01(0x77)
 
 while True:    
-    time.sleep(1)
+    time.sleep(5)
     os.system("clear")
     temperature  = sensor.readTemp()
-    temperatureString   = str(temperature) + "\r\n"
+    timeStr = str(datetime.datetime.now())
+    temperatureString   = timeStr + ";" + str(temperature) +  "\r\n"
     with open('log','a') as f:
         f.write (temperatureString)
     print 'Temperature = %3.3f C' % temperature
